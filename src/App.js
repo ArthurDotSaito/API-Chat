9d7fb1731e8d900e95ee7f9a1c1c89a883c1b93f
Arthur Saito <arthur_saito1998@hotmail.com>
2023-01-14 12:25:05 -0300

    endpoint /participants seems working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import {MongoClient} from 'mongodb';
 import { userSchema } from './tools/validation.js';
+import { timeFormat } from './tools/getTimeInFormat.js'
 
 const server = express();
 const PORT = 5000;
@@ -30,5 +31,22 @@ server.post('/participants', async (req, res) =>{
     const userdata = {...req.body, lastStatus: Date.now()};
     const {value: userRegister, error} = userSchema.validate(userdata.name);
     if(error) return res.status(422).send("Usuário não pode ser vazio e deve possuir caracteres de A-Z");
-
-})
+    try{
+        const userNameExist = await db.collection('participants').findOne({name: userdata.name});
+        if(!userNameExist){
+            const entryMessageStatus = {
+                from: userdata.name,
+                to: 'Todos',
+                text:'Entra na sala...',
+                type:'status',
+                time: timeFormat()
+            };
+            await db.collection('messages').insertOne(entryMessageStatus);
+            await db.collection('participants').insertOne(userdata);
+        }else{
+            res.sendStatus(409);
+        }
+    }catch(error){
+        console.log(error);
+    }
+});
---
+++ src/tools/getTimeInFormat.js
@@ -0,0 +1,5 @@
+import dayjs from 'dayjs';
+
+export const timeFormat = () =>{
+    return dayjs.format('HH:mm:ss');
+}
